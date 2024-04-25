c0bc01fb42bc64dc65b6ab00a428c21f224a294c
pekellogg <peyton846@revature.net>
2024-04-24 20:52:14 -0700

    adds Navigation component w/ minimal styling, adds components as pages: owners, renters, home

---
+++ frontend/src/ui/renters-page/index.tsx
@@ -0,0 +1,12 @@
+import RentersContainer from '../../components/renters/RentersContainer';
+
+function RentersPage() {
+  return (
+    <>
+        <h1>Renters</h1>
+        <RentersContainer />
+    </>
+  );
+}
+
+export default RentersPage;
