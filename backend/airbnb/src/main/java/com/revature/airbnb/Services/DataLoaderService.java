3be836bca14e94bfd80dc184475153ac7d8ab23e
pekellogg <peyton846@revature.net>
2024-04-29 14:14:45 -0700

    updates DataLoaderService to iterate over ArrayList (from JsonNode)

--- backend/airbnb/src/main/java/com/revature/airbnb/Services/DataLoaderService.java
+++ backend/airbnb/src/main/java/com/revature/airbnb/Services/DataLoaderService.java
@@ -1,14 +1,19 @@
 package com.revature.airbnb.Services;
 
 import com.revature.airbnb.Services.OwnerService;
+import com.revature.airbnb.Models.Listing;
 import com.revature.airbnb.Services.ListingService;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.asm.TypeReference;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@Component
 public class DataLoaderService implements CommandLineRunner {
 
     private final ObjectMapper om;
@@ -24,16 +29,21 @@ public class DataLoaderService implements CommandLineRunner {
     /* following tutorial at time: https://youtu.be/EumLbf8WjnY?t=823 */
     @Override
     public void run(String... args) throws Exception {
-        JsonNode jsonNode;
-        try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/seed-data.json")) {
-            jsonNode = om.readValue(inputStream, JsonNode.class);
+        // List<Listing> listings = new ArrayList<>();
+        List<JsonNode> listings = new ArrayList<>();
+        try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/seed-Data.json")) {
+            listings = om.readValue(inputStream, om.getTypeFactory().constructCollectionType(List.class, Listing.class));
         } catch (Exception e) {
             throw new RuntimeException("Failed to load seed data", e);
         }
-        JsonNode listings = getListings(jsonNode);
-    }
-    
-    private JsonNode getListings(JsonNode jsonNode) {
-        return jsonNode.get("listings");
+
+        // for (int i = 0; i < listings.size(); i++) {
+        //     System.out.println(listings.get(i));
+        // }
+        // for (int i = 0; i < 5; i++) {
+        //     String l = listings.get(i).asText();
+        //     // System.out.println(l);
+        //     // System.out.println(listings.get(i).toString());
+        // }
     }
 }
