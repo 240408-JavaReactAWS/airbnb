272c9610889e6712acfb5fb06266143e4219300f
pekellogg <peyton846@revature.net>
2024-04-30 16:33:16 -0700

    FE: removes unnecessary nesting with ui dir

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,9 @@ import OwnersPage from './ui/owners-page/index';
 import RentersPage from './ui/renters-page';
 import Navigation from './ui/nav/Navigation';
 import Home from './ui/home-page/index';
-import LoginPage from './ui/login-page/login';
-import RegisterPage from './ui/register-page/register';
-import LogoutPage from './ui/logout-page/logout';
+import LoginPage from './ui/login-page';
+import RegisterPage from './ui/register-page';
+import LogoutPage from './ui/logout-page';
 
 
 
@@ -22,7 +22,7 @@ function App() {
 
           {/* create logout path if user is logged in */}
           {localStorage.hasOwnProperty("user") && <Route path='/logout' element={<LogoutPage />} />}
-          
+
           <Route path='/owners' element={<OwnersPage />} />
           <Route path='/renters' element={<RentersPage />} />
           <Route path='*' element={<h1>404 Not Found</h1>}></Route>
--- frontend/src/ui/logout-page/index.tsx
+++ frontend/src/ui/logout-page/index.tsx
@@ -1,4 +1,4 @@
-import LogoutButton from "../LogoutButton"
+import LogoutButton from "./LogoutButton"
 import { useParams } from "react-router-dom";
 
 function LogoutPage() {
