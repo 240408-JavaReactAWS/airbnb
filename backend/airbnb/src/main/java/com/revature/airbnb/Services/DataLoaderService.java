b9749ec7d409efea2177c9b21124217670e3b9f0
pekellogg <peyton846@revature.net>
2024-04-29 19:31:14 -0700

    adds functional DataLoaderService to populate 30 Owners and Listings data

--- backend/airbnb/src/main/java/com/revature/airbnb/Services/DataLoaderService.java
+++ backend/airbnb/src/main/java/com/revature/airbnb/Services/DataLoaderService.java
@@ -1,49 +1,96 @@
 package com.revature.airbnb.Services;
 
-import com.revature.airbnb.Services.OwnerService;
 import com.revature.airbnb.Models.Listing;
-import com.revature.airbnb.Services.ListingService;
-import java.io.InputStream;
+import com.revature.airbnb.Models.Owner;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import org.springframework.asm.TypeReference;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class DataLoaderService implements CommandLineRunner {
-
-    private final ObjectMapper om;
     private final OwnerService os;
     private final ListingService ls;
 
-    public DataLoaderService(ObjectMapper om, OwnerService os, ListingService ls) {
+    DataLoaderService(OwnerService os, ListingService ls) {
         this.os = os;
         this.ls = ls;
-        this.om = om;
     }
 
-    /* following tutorial at time: https://youtu.be/EumLbf8WjnY?t=823 */
     @Override
     public void run(String... args) throws Exception {
-        // List<Listing> listings = new ArrayList<>();
-        List<JsonNode> listings = new ArrayList<>();
-        try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/seed-Data.json")) {
-            listings = om.readValue(inputStream, om.getTypeFactory().constructCollectionType(List.class, Listing.class));
-        } catch (Exception e) {
+        JSONArray jsonArray;
+        try {
+            String content = new String(Files.readAllBytes(Paths.get(getClass().getResource("/data/seed-Data.json").toURI())));
+            jsonArray = new JSONArray(content);
+        } catch (IOException | URISyntaxException e) {
             throw new RuntimeException("Failed to load seed data", e);
         }
 
-        // for (int i = 0; i < listings.size(); i++) {
-        //     System.out.println(listings.get(i));
-        // }
-        // for (int i = 0; i < 5; i++) {
-        //     String l = listings.get(i).asText();
-        //     // System.out.println(l);
-        //     // System.out.println(listings.get(i).toString());
-        // }
+        // traverse array of objects
+        // each object represents a Listing
+        // each Listing has a primaryHost key points to object; represents an Owner
+        for (int i = 0; i < 30; i++) {
+            // get listing
+            JSONObject lObj = jsonArray.getJSONObject(i);
+
+            // get owner
+            JSONObject ownerObj = lObj.getJSONObject("primaryHost"); 
+
+            // create new owner
+            Owner newOwner = os.registerOwner(
+                // make name unique
+                ownerObj.getString("firstName").concat(Integer.toString(i)),
+                "password",
+                "example@email.com"
+            );
+
+            // listing's description
+            JSONObject descObj = lObj.getJSONObject("sectionedDescription");
+
+            // check if summary exists
+            String desc;
+            if (descObj.has("summary")) {
+                desc = descObj.getString("summary");
+            } else if (descObj.has("description")) {
+                desc = descObj.getString("description");
+            } else {
+                desc = "No description available.";
+            }
+
+            // truncate description if too long
+            if (desc.length() > 255) {
+                desc = desc.substring(0, 255);
+            }
+
+            // listing's photos
+            JSONArray jsonArrayPhotos = lObj.getJSONArray("photos");
+            List<String> photos = new ArrayList<>();
+
+            // collect photos
+            for (int j = 0; j < jsonArrayPhotos.length(); j++) {
+                photos.add(jsonArrayPhotos.getJSONObject(j).getString("pictureUrl"));
+            }
+
+            // create new listing
+            Listing newListing = new Listing(
+                lObj.getString("address"),
+                lObj.getString("city"),
+                lObj.getString("state"),
+                desc,
+                photos,
+                lObj.getString("name"),
+                newOwner.getUserId()
+            );
+
+            // persist to DB
+            ls.createListing(newListing);
+        }
     }
 }
