637fda94ea30a0362767dd17a9888f0dba900856
pekellogg <peyton846@revature.net>
2024-05-24 16:28:29 -0700

    removes logout page, adds logout button to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,24 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import OwnersPage from './ui/owners-page/index';
-import RentersPage from './ui/renters-page';
 import Navigation from './ui/nav/Navigation';
 import Home from './ui/home-page/index';
 import LoginPage from './ui/login-page';
 import RegisterPage from './ui/register-page';
-import LogoutPage from './ui/logout-page';
 import OwnerListings from './ui/owners-page/OwnerListings';
 import RenterRequestedListings from './ui/renters-page/RenterRequestedListings';
+import LogoutButton from './ui/logout-page/LogoutButton';
 
 function App() {
   return (
     <>
+      {localStorage.hasOwnProperty("user") ? <LogoutButton /> : <></>}
       <BrowserRouter>
         <Navigation />
         <Routes>
           <Route path='/' element={<Home />} />
 
-          {/* create mylistings if logged in user is an owner */}
           {(localStorage.hasOwnProperty("role") && localStorage.getItem("role") === "owner") && <Route path="/listings" element={<OwnerListings />} />}
 
-          {/* create mylistings if logged in user is an owner */}
           {(localStorage.hasOwnProperty("role") && localStorage.getItem("role") === "renter") && <Route path="/listings" element={<RenterRequestedListings />} />}
           
           {/* show register if no user logged in */}
@@ -29,10 +26,7 @@ function App() {
 
           {/* create login path when no user logged in */}
           {!localStorage.hasOwnProperty("user") && <Route path='/login' element={<LoginPage />} />}
-
-          {/* create logout path if user is logged in */}
-          {localStorage.hasOwnProperty("user") && <Route path='/logout' element={<LogoutPage />} />}
-
+        
           <Route path='*' element={<h1>404 Not Found</h1>}></Route>
         </Routes>
       </BrowserRouter>
--- frontend/src/ui/nav/Navigation.tsx
+++ frontend/src/ui/nav/Navigation.tsx
@@ -20,24 +20,21 @@ function Navigation() {
 
   return (
     <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
+      <ul>
+        <li><Link to="/">Home</Link></li>
 
-          {/* show mylistings if logged in user is an owner */}
-          {(localStorage.hasOwnProperty("role") && localStorage.getItem("role") === "owner") && <li><Link to="/listings" >My Listings</Link></li>}
+        {/* show mylistings if logged in user is an owner */}
+        {(localStorage.hasOwnProperty("role") && localStorage.getItem("role") === "owner") && <li><Link to="/listings" >My Listings</Link></li>}
 
-          {/* show my-requested-listings if logged in user is a renter */}
-          {(localStorage.hasOwnProperty("role") && localStorage.getItem("role") === "renter") && <li><Link to="/listings" >My Requests</Link></li>}
+        {/* show my-requested-listings if logged in user is a renter */}
+        {(localStorage.hasOwnProperty("role") && localStorage.getItem("role") === "renter") && <li><Link to="/listings" >My Requests</Link></li>}
 
-          {/* show register if no user logged in */}
-          {!localStorage.hasOwnProperty("user") && <li><Link to="/register">Register</Link></li>}
+        {/* show register if no user logged in */}
+        {!localStorage.hasOwnProperty("user") && <li><Link to="/register">Register</Link></li>}
 
-          {/* hide login when user already logged in */}
-          {!localStorage.hasOwnProperty("user") && <li><Link to="/login">Login</Link></li>}
-
-          {/* show logout if user is logged in */}
-          {localStorage.hasOwnProperty("user") && <li><Link to="/logout">Logout</Link></li>}
-        </ul>
+        {/* hide login when user already logged in */}
+        {!localStorage.hasOwnProperty("user") && <li><Link to="/login">Login</Link></li>}
+      </ul>
     </nav>
   )
 }
