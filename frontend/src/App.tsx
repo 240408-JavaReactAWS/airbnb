051281c869f258be4aebfeb41557d19b875b5669
pekellogg <peyton846@revature.net>
2024-05-27 16:45:54 -0700

    refactors App & Navigation for brevity, removes token property from IUser

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { validateLogin } from './shared/utils/ValidateLogin';
 import Navigation from './ui/nav/Navigation';
@@ -10,38 +10,42 @@ import ListingsContainer from './components/listings/ListingsContainer';
 import './App.css';
 
 function App() {
+  // TODO: encapsulate in single state object
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [currentUser, setCurrentUser] = useState();
+  const [currentUser, setCurrentUser] = useState<{} | null>(null); // TODO: leverage IUser + IRenter OR IOwner
+  const [role, setRole] = useState<String | null>(null);
 
+  // TODO: lift useEffect to grab current user surrounding App component; useContext?
   useEffect(() => {
     let asyncCall = async () => {
       let validateSession = await validateLogin.validateSession();
       if (validateSession) {
         setIsLoggedIn(true);
-        setCurrentUser
+        let user = localStorage.getItem("user")
+        if (user) {
+          setCurrentUser(JSON.parse(user))
+          setRole(localStorage.getItem("role"))
+          setIsLoggedIn(true)
+        }
       } else {
-        setIsLoggedIn(false);
+        setIsLoggedIn(false)
       }
     }
     asyncCall();
-  }, [localStorage.getItem("user")]);
+  }, []); // TODO: add dependencies such that app properly re-renders post user login
 
   return (
     <>
-      <BrowserRouter>
-      <Navigation />
-      {localStorage.hasOwnProperty("user") ? <LogoutButton /> : <></>}
-      
-      <Routes>
-        <Route path='/' element={<Home />} />
-
-        {(localStorage.hasOwnProperty("role") && localStorage.getItem("role") === "owner") && <Route path="/listings" element={<OwnerListings />} />}
-
-        {(localStorage.hasOwnProperty("role") && localStorage.getItem("role") === "renter") && <Route path="/listings" element={<RenterRequestedListings />} />}
-      
-        <Route path='*' element={<h1>404 Not Found</h1>}></Route>
-      </Routes>
-      </BrowserRouter>
+      <Router>
+        <Navigation />
+        {currentUser ? <LogoutButton /> : <></>}
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {currentUser && <Route path="/listings" element={ role === "owner" ? <OwnerListings /> : <RenterRequestedListings />} />} {/* TODO: understand why no re-render post user logout */}
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </Router>
       <ListingsContainer />
     </>
   )
--- frontend/src/ui/nav/Navigation.tsx
+++ frontend/src/ui/nav/Navigation.tsx
@@ -5,20 +5,15 @@ import './Navigation.css';
 
 function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
-
   const toggle = () => setIsOpen((prev) => !prev)
 
   return (
     <nav>
       <ul>
         <li><Link to="/">Home</Link></li>
-
-        {/* show mylistings if logged in user is an owner */}
-        {(localStorage.hasOwnProperty("role") && localStorage.getItem("role") === "owner") && <li><Link to="/listings" >My Listings</Link></li>}
-
-        {/* show my-requested-listings if logged in user is a renter */}
-        {(localStorage.hasOwnProperty("role") && localStorage.getItem("role") === "renter") && <li><Link to="/listings" >My Requests</Link></li>}
+        {<li><Link to="/listings" >{(localStorage.getItem("role") === "owner") ? "My Listings" : "My Requests"}</Link></li>}
       </ul>
+      {/* TODO: conditionally render Register or Login */}
       <button onClick={toggle}>Register or Login</button>
       {isOpen && <RegisterLogin />}
     </nav>
